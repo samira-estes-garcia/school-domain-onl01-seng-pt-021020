d1d2d7b5bfed6fda57d061d6bf10aa86ae728974
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-02-24 22:17:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  
+  attr_accessor :roster
+  attr_reader :name
+  
+  ROSTER = []
+  
+  def initialize(name)
+    @name = name
+    @roster = []
+  end
+  
+  def add_student(student_name, grade)
+  end
+  
+  def grade
+  end
+  
+  def sort 
+  end
+end
